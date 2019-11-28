40213d5739ef6a1b625acd8b7d6167266b55c1de
Jerome Laban <jerome.laban@nventive.com>
2019-11-28 14:59:45 -0500

    Update cli search

--- build/gitpod/serve-remote-control.sh
+++ build/gitpod/serve-remote-control.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-RC_HOST_PATH=${dirname `ls -rt /workspace/.nuget/uno.ui.remotecontrol/*/tools/rc/host/*Host.dll | head -1`}
+LATEST_RC=`ls -rt /workspace/.nuget/uno.ui.remotecontrol/*/tools/rc/host/*Host.dll | head -1`
+RC_HOST_PATH=`dirname $LATEST_RC`
 
 pushd $RC_HOST_PATH
 
-dotnet run --httpPort 53487
+dotnet Uno.UI.RemoteControl.Host.dll --httpPort 53487
 
 popd
--- build/gitpod/serve-sampleapp-wasm.sh
+++ build/gitpod/serve-sampleapp-wasm.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
+pushd /workspace/Uno.QuickStart/src/MyApp.Wasm/bin/Debug/netstandard2.0/dist
 
-CLI_HOST_PATH=${dirname `ls -rt /workspace/.nuget/uno.wasm.bootstrap.cli/*/dotnet-unowasm.dll | head -1`}
-
-pushd $(CLI_HOST_PATH)
-
-dotnet unowasm serve --urls=http://*:8000 --pathbase /workspace/uno/src/MyApp.Wasm/bin/Debug/netstandard2.0/dist
+python3 server.py
 
 popd
+
